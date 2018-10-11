6fbf73f628650f699faff1dfbd548061105236e9
techno <amitavaaditya2009@gmail.com>
2018-10-12 01:42:48 +0530

    Developed unit test cases

--- test.py
+++ test.py
@@ -1,31 +1,113 @@
-from keras.models import load_model
-from keras.preprocessing.image import load_img, img_to_array
-import numpy as np
+import unittest
+import os
+import shutil
+import requests
+from PIL import Image
 
 
-def load_image(img_src):
-    image = load_img(img_src, target_size=(150, 150))
-    img_tensor = img_to_array(image)
-    img_tensor = np.expand_dims(img_tensor, axis=0)
-    img_tensor /= 255.
-    return img_tensor
-
-def predict(model, image):
-    classes = {
-        0: '0',
-        1: '180',
-        2: '270',
-        3: '90'
-    }
-    prediction = model.predict(image)
-    print(prediction)
-    predicted_class = classes[np.argmax(prediction)]
-    return predicted_class
+IMAGE_PATH = 'final/test/0/Deniz_Baykal_0001.jpg'
+KERAS_REST_API_PREDICT_URL = "http://localhost:5000/predict"
+KERAS_REST_API_CORRECT_URL = "http://localhost:5000/correct"
 
 
-if __name__ == '__main__':
-    model_src = 'model.h5'
-    model = load_model(model_src)
-    img_src = 'final/test/180/Aaron_Peirsol_0001.jpg'
-    image = load_image(img_src)
-    print(predict(model, image))
+class ModelTest(unittest.TestCase):
+
+    def setUp(self):
+        temp_test_dir = 'temp_test'
+        if os.path.exists(temp_test_dir):
+            shutil.rmtree(temp_test_dir)
+        os.mkdir(temp_test_dir)
+        image = Image.open(IMAGE_PATH)
+        for rotation in (0, 90, 180, 270):
+            image.rotate(rotation).save(
+                os.path.join(temp_test_dir, '{}.jpg'.format(rotation))
+            )
+
+    def tearDown(self):
+        temp_test_dir = 'temp_test'
+        if os.path.exists(temp_test_dir):
+            shutil.rmtree(temp_test_dir)
+
+    def test_rotation0_predict(self):
+        with open('temp_test/0.jpg', "rb") as f:
+            image = f.read()
+        payload = {"image": image}
+        r = requests.post(KERAS_REST_API_PREDICT_URL, files=payload).json()
+        predicted_label = r['final_label']
+        original_label = 0
+        self.assertEqual(predicted_label, original_label)
+
+    def test_rotation90_predict(self):
+        with open('temp_test/90.jpg', "rb") as f:
+            image = f.read()
+        payload = {"image": image}
+        r = requests.post(KERAS_REST_API_PREDICT_URL, files=payload).json()
+        predicted_label = r['final_label']
+        original_label = 90
+        self.assertEqual(predicted_label, original_label)
+
+    def test_rotation180_predict(self):
+        with open('temp_test/180.jpg', "rb") as f:
+            image = f.read()
+        payload = {"image": image}
+        r = requests.post(KERAS_REST_API_PREDICT_URL, files=payload).json()
+        predicted_label = r['final_label']
+        original_label = 180
+        self.assertEqual(predicted_label, original_label)
+
+    def test_rotation270_predict(self):
+        with open('temp_test/270.jpg', "rb") as f:
+            image = f.read()
+        payload = {"image": image}
+        r = requests.post(KERAS_REST_API_PREDICT_URL, files=payload).json()
+        predicted_label = r['final_label']
+        original_label = 270
+        self.assertEqual(predicted_label, original_label)
+
+    def test_rotation0_correct(self):
+        with open('temp_test/0.jpg', "rb") as f:
+            image = f.read()
+        payload = {"image": image}
+        r = requests.post(KERAS_REST_API_CORRECT_URL, files=payload)
+        r.raw.decode_content = True
+        corrected_image = r.content
+        payload = {"image": corrected_image}
+        r = requests.post(KERAS_REST_API_PREDICT_URL, files=payload).json()
+        predicted_label = r['final_label']
+        self.assertEqual(predicted_label, 0)
+
+    def test_rotation90_correct(self):
+        with open('temp_test/90.jpg', "rb") as f:
+            image = f.read()
+        payload = {"image": image}
+        r = requests.post(KERAS_REST_API_CORRECT_URL, files=payload)
+        r.raw.decode_content = True
+        corrected_image = r.content
+        payload = {"image": corrected_image}
+        r = requests.post(KERAS_REST_API_PREDICT_URL, files=payload).json()
+        predicted_label = r['final_label']
+        self.assertEqual(predicted_label, 0)
+
+    def test_rotation180_correct(self):
+        with open('temp_test/180.jpg', "rb") as f:
+            image = f.read()
+        payload = {"image": image}
+        r = requests.post(KERAS_REST_API_CORRECT_URL, files=payload)
+        r.raw.decode_content = True
+        corrected_image = r.content
+        payload = {"image": corrected_image}
+        r = requests.post(KERAS_REST_API_PREDICT_URL, files=payload).json()
+        predicted_label = r['final_label']
+        self.assertEqual(predicted_label, 0)
+
+    def test_rotation270_correct(self):
+        with open('temp_test/270.jpg', "rb") as f:
+            image = f.read()
+        payload = {"image": image}
+        r = requests.post(KERAS_REST_API_CORRECT_URL, files=payload)
+        r.raw.decode_content = True
+        corrected_image = r.content
+        payload = {"image": corrected_image}
+        r = requests.post(KERAS_REST_API_PREDICT_URL, files=payload).json()
+        predicted_label = r['final_label']
+        self.assertEqual(predicted_label, 0)
