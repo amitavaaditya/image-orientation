ea4b7269ccf289aa0d89b01bc4754a96c5fb16e7
techno <amitavaaditya2009@gmail.com>
2018-10-11 23:24:46 +0530

    Updated RESTful API

--- main.py
+++ main.py
@@ -4,11 +4,10 @@ import numpy as np
 import flask
 from PIL import Image
 import io
-# import tensorflow as tf
+
 
 app = flask.Flask(__name__)
 model = None
-graph = None
 
 
 def prepare_image(image, target):
@@ -19,6 +18,13 @@ def prepare_image(image, target):
     image = np.expand_dims(image, axis=0)
     image /= 255.
     return image
+
+
+def make_prediction(image):
+    image_tensor = prepare_image(image, target=(150, 150))
+    predictions = model.predict(image_tensor)
+    results, final_label = decode_prediction(predictions[0])
+    return results, final_label
 
 
 def decode_prediction(predictions):
@@ -35,17 +41,28 @@ def predict():
         if flask.request.files.get("image"):
             image = flask.request.files["image"].read()
             image = Image.open(io.BytesIO(image))
-            image_tensor = prepare_image(image, target=(150, 150))
-            predictions = model.predict(image_tensor)
             data["predictions"] = []
-            results, final_label = decode_prediction(predictions[0])
+            results, _ = make_prediction(image)
             for label, probability in results:
                 r = {'label': label, 'probability': float(probability)}
                 data['predictions'].append(r)
-            rotation = int(final_label)
-            rotated_image = rotate_image(image, rotation)
             data["success"] = True
     return flask.jsonify(data)
+
+
+@app.route("/correct", methods=['POST'])
+def correct():
+    if flask.request.method == 'POST':
+        if flask.request.files.get("image"):
+            image = flask.request.files["image"].read()
+            image = Image.open(io.BytesIO(image))
+            _, final_label = make_prediction(image)
+            rotation = int(final_label)
+            rotated_image = rotate_image(image, rotation)
+            rotated_image.save('converted_image.jpeg', 'JPEG')
+    return flask.send_file('converted_image.jpeg',
+                           mimetype='image/jpeg',
+                           as_attachment=True)
 
 
 def rotate_image(image, rotation):
@@ -54,10 +71,8 @@ def rotate_image(image, rotation):
 
 def model_load():
     global model
-    # global graph
     model_src = 'model.h5'
     model = load_model(model_src)
-    # graph = tf.get_default_graph()
     model._make_predict_function()
 
 
